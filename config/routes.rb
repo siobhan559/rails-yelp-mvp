4792c60671c3a6d386fc532f080698ffe6a07432
Siobhan <siobhanbrown559@gmail.com>
2021-11-09 18:14:46 +0000

    refactoring finished

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # get 'restaurants/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
 end
